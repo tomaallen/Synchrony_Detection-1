86d57e12f1d6da284b07416adcc93b6122019ed9
Tom Allen <taallen47@gmail.com>
2023-10-09 09:46:43 +0100

    Deleting excess files
    Delete 2_Reaching-Detection/src/draft.py

--- 2_Reaching-Detection/src/draft.py
+++
@@ -1,2 +0,0 @@
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-ffmpeg_extract_subclip("data/c006/20220310104945.MTS", 1*3600+48*60, 1*3600+53*60, targetname="data/c006/20220310104945_stt.avi")
